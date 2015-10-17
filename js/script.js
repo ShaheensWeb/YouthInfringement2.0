10965f4e6918b46dad1828728d5280add7b25218
Shaheen Ghazazani <shaheenghazazani@gmail.com>
2015-10-16 21:35:24 -0400

    making it a real website

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// script.js
+
+    // create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+    // configure our routes
+    scotchApp.config(function($routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+			.when('/festival', {
+                templateUrl : 'pages/festival.html',
+                controller  : 'festivalController'
+            })
+
+            .when('/getInvolved', {
+                templateUrl : 'pages/getInvolved.html',
+                controller  : 'getInvolvedController'
+            })
+
+            .when('/theTeam', {
+                templateUrl : 'pages/team.html',
+                controller  : 'theTeamController'
+            })
+
+            .when('/workshop', {
+                templateUrl : 'pages/workshop.html',
+                controller  : 'workshopController'
+            })
+
+            .when('/theShows', {
+                templateUrl : 'pages/theShows.html',
+                controller  : 'theShowsController'
+            })
+
+            .when('/schedule', {
+                templateUrl : 'pages/schedule.html',
+                controller  : 'scheduleController'
+            })
+
+            .when('/pastEvents', {
+                templateUrl : 'pages/pastEvents.html',
+                controller  : 'pastEventsController'
+            })
+
+            .when('/contactUs', {
+                templateUrl : 'pages/contactUs.html',
+                controller  : 'contactUsController'
+            })
+
+            .otherwise({
+            	redirectTo: '/'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    scotchApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+	scotchApp.controller('festivalController', function($scope) {
+        $scope.message = 'Look! I am an festival controller.';
+    });
+
+    scotchApp.controller('getInvolvedController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+
+    scotchApp.controller('theTeamController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+ 	scotchApp.controller('workshopController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+    scotchApp.controller('theShowsController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+    scotchApp.controller('scheduleController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+    scotchApp.controller('pastEvents', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+    scotchApp.controller('contactUsController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
